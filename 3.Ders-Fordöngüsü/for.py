b9fafb4f1c482f1788a35cfe5ec32a7a73483b15
zafergunayonalan <zafergunay7@gmail.com>
2023-10-29 01:27:50 +0300

    For Döngüsü

---
+++ 3.Ders-Fordöngüsü/for.py
@@ -0,0 +1,58 @@
+
+# ! 1'den 10'a kadar olan sayıları yazdıralım
+
+# for i in range(1,11,1):
+#     print(i)
+
+# ! Örnek
+# Merhaba
+# Merhaba Merhaba
+# Merhaba Merhaba Merhaba
+# Merhaba Merhaba Merhaba Merhaba
+# Merhaba Merhaba Merhaba Merhaba Merhaba
+
+# for i in range(1,6,1):
+#     print(i * " " "Merhaba")
+
+# ! Örnek
+# *
+# **
+# ***
+# ****
+# *****
+# ****
+# ***
+# **
+# *
+
+# for i in range(1,6,1):
+#     print("*" * i)
+# for j in range(6,0,-1):
+#     print("*" * j)
+
+
+# ! Örnek
+# ***************(15 adet)
+# *
+# *
+# *
+# **********(10 adet)
+# *
+# *
+# *
+# *
+# *
+
+# for i in range(1,11):
+#     if i == 1:
+#         print("*" * 15)
+#     elif i == 5:
+#         print("*" * 10)
+#     elif i < 5 or i > 5:
+#         print("*" * 1)
+# print("*"*15)
+# for i in range(1,3):
+#     print("*" * 1)
+# print("*" * 10)
+# for i in range(1,5):
+#     print("*" * 1)
