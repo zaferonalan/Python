460bf095d869509cc4d0655458194dbfd187dc73
zafergunayonalan <zafergunay7@gmail.com>
2023-10-28 23:20:36 +0300

    VeriTipleri

---
+++ 1.Ders-Degiskenler/veritipleri.py
@@ -0,0 +1,58 @@
+# print("Hello, Word")
+
+# name = "Zafer Günay Önalan"
+# print(name)
+
+# name = "Zafer Günay";
+# surnanme = "Önalan";
+# age = 25;
+
+# print(name + surnanme)
+# print (name + " " + surnanme)
+# print(name,surnanme)
+# print(name + surnanme + str(age))
+
+# print(f"Merhaba ismim {name} soyismim {surnanme}. Ben {age} yaşındayım ")
+
+# print(f"""
+#     Merhaba
+#     Benim adım {name}
+#     Soyadım {surnanme}
+#     Ben {age} yaşındaym """)
+
+
+# ! String Veri Türleri
+# name = "Zafer Önalan"
+# # Todo: Yazıları küçültür.
+# result = name.casefold()
+# print(result)
+
+# print(name.split(" "))
+
+# print(len(name))
+# print(name[0].lower())
+
+# print(name.upper())
+# Todo: İlk harfi büyük sonrası küçük 
+# print(name.capitalize())
+# print(name.title())
+# Todo: Değer varsa 1 yoksa 0 döndürür
+# print(name.lower().count("zafer"))
+
+# print(name.lower().count("Önalan"))
+# print(name.replace("Zafer","Merve"))
+
+
+# ! İnteger Veri Tipi
+# print(5 / 3)
+# print(type(5/3))
+
+# Todo: pozitif sayıya çevirir
+# number = -3;
+# print(abs(number))
+
+# number2 = 6.5367564
+# print(round(number2,4))
+# print(max(12,20,30))
+# print(min(12,20,30))
+# print(pow(2,4))
