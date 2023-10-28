37cb8eadf16c56afadadbb19e92c78c910ccfcfb
zafergunayonalan <zafergunay7@gmail.com>
2023-10-29 01:16:08 +0300

    İf-Else

---
+++ 2.Ders-DögülerVeinput/Döngüler-if-Else.py
@@ -0,0 +1,39 @@
+
+# ! input
+# name = input("İsim Giriniz :")
+# surname = input("Soyisim Giriniz :")
+# age = int(input("Yaşınızı Giriniz :"))
+
+# print(name + " " + surname + " " + str(age))
+
+
+# ! İf-Else
+
+# number = int(input("Bir sayı Giriniz :"))
+
+# if number % 2 == 0 and number > 0 :
+#     print(f"{number} çift sayıdır")
+# elif number % 2 != 0 and number > 0:
+#     print(f"{number} Tek sayıdır.")
+# elif number < 0 :
+#     print(f"{number} Negatif sayıdır.")
+# else:
+#     print(f"{number} Sıfır")
+
+
+# result = int(input("Notunuzu Giriniz :"))
+
+# if result < 100 and result >= 85:
+#     print(f"Notunuz : {result} Harf notu : A ")
+# elif result < 85 and result >= 70:
+#     print(f"Notunuz : {result} Harf notu : B ")
+# elif result < 70 and result >= 55:
+#     print(f"Notunuz : {result} Harf notu : C ")
+# elif result < 55 and result >= 45:
+#     print(f"Notunuz : {result} Harf notu : D ")
+# elif result < 45 and result >= 30:
+#     print(f"Notunuz : {result} Harf notu : E ")
+# elif result < 35 and result >= 0:
+#     print(f"Notunuz : {result} Harf notu : F ")
+# else :
+#     print("100-0 Arasında değer giriniz")        
